5ef8b47fd6f11778d0ed8e45a2625ee73340bcff
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-04-11 20:35:07 -0400

    Scanner
    Recuerden hacer npm installl

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// Plugin nativo de codigos de barra
+import {BarcodeScanner} from 'ionic-native';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,26 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  click() 
+  {
+    BarcodeScanner.scan()
+    .then((result) => {
+      if (!result.cancelled) 
+      {
+        const barcodeData = new BarcodeData(result.text, result.format);
+        this.scanDetails(barcodeData);
+
+        // Quitar en Produccion:
+        console.log("Scanned exitoso");
+        console.log(barcodeData);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      alert(err);
+    })
   }
 
 }
