309493ccd0b60649586c75a01108531331332192
Rocco Tarantino <roccotc@gmail.com>
2017-04-15 12:02:11 -0400

    Probando subscribe

--- src/pages/pagar-fase2/pagar-fase2.ts
+++ src/pages/pagar-fase2/pagar-fase2.ts
@@ -13,21 +13,32 @@ user: FirebaseObjectObservable<any>;
 user2: FirebaseObjectObservable<any>;
 clave:any;
 monto:any;
+clavep:any;
 
 
   constructor(public navCtrl: NavController, public alertController: AlertController, public navParams: NavParams, public database: AngularFireDatabase) {
       
        this.user = this.database.object('/User/-KhUxoaDXsMYe0x1-Ggf');
        this.user2 = this.database.object('/User/-KhUY7ugwy_VJqJXIDay');
-       
-      
+
+
   }
 
   pagar() {
     // Clave introducidad por el usuario. Esta clave se debe comparar con ClavePago de Firebase
-    this.user.subscribe(snapshot => console.log(snapshot.ClavePago));
-    console.log("Clave: "+this.clave);
-    console.log("Monto: "+this.monto);
+      this.user.subscribe(
+        snapshot => {
+            (this.clavep) = snapshot.ClavePago;
+            console.log(this.clavep);
+            console.log("Clave: "+this.clave);
+            console.log("Monto: "+this.monto);   
+        },
+        err => {
+          console.log("Oops!");
+        }
+      );
+    
+     
   }
 
 }
