997c987e32c6446fd3ae59fa1000668b66794c97
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-04-25 21:11:43 -0400

    Pagar fase 1 done

--- src/pages/pagar-fase1/pagar-fase1.ts
+++ src/pages/pagar-fase1/pagar-fase1.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,  AlertController } from 'ionic-angular';
 import { PagarFase2 } from "../pagar-fase2/pagar-fase2";
-import { FirebaseObjectObservable, AngularFireDatabase } from "angularfire2";
+import { FirebaseObjectObservable, AngularFireDatabase, FirebaseListObservable } from "angularfire2";
 
 
 /**
@@ -19,9 +19,10 @@ export class PagarFase1 {
 
   // Atributos
   private usuarioApagar: any;
+  private Contactos: FirebaseListObservable<any>;
 
   // Constructor
-  constructor(public navCtrl: NavController, public navParams: NavParams, public database: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public database: AngularFireDatabase,  private alertController: AlertController) {
     this.usuarioApagar = navParams.get('usuarioApagar'); //Inicializa el usuario escaneado con la data pasada del home.ts
     
   }
@@ -36,7 +37,34 @@ export class PagarFase1 {
 
   private guardarEnContactos()
   {
+
+    this.Contactos = this.database.list('/Contactos/-KhUY7ugwy_VJqJXIDay'); // Cambiar en Login
+     this.Contactos.push({
+                  Nombre: this.usuarioApagar.Nombre,
+                  Nickname: "",
+                  Telefono: this.usuarioApagar.Telefono,
+                  Cuentas:{Cuenta1: {Numero: this.usuarioApagar.Cuentas.Cuenta1.Numero, Banco: this.usuarioApagar.Cuentas.Cuenta2.Numero}},
+                  Apellido: this.usuarioApagar.Apellido,
+                  Correo: this.usuarioApagar.Correo
+              }).catch((err: any) => {
+                let error = this.alertController.create({
+                  title: "Error",
+                  message: "Presentamos un error al procesar el pago, por favor intentelo nuevamente. Verifique su conexión a internet por favor. ",
+                  buttons: [
+                  {
+                    text: "Ok",
+                    handler: data => {
+                    console.log(' ');
+                    }
+                  }
+                  ]
+              });
+                return ;
+            });
+
     // Aqui guarda usuarioApagar como un contacto nuevo (Usar codigo de rocco)
+
+
   }
 
   ionViewDidLoad() {
