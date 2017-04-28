0505e6cddb0d3bfd19e0f50b112a01368df8780a
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-04-28 00:45:37 -0400

    Sprint 1 Terminado :100:
    Prototipo Alpha 0.1  :white_check_mark: /play tada
    - Proceso de pago : Done.

--- src/pages/pagar-fase2/pagar-fase2.ts
+++ src/pages/pagar-fase2/pagar-fase2.ts
@@ -10,41 +10,49 @@ import { Tabs } from '../tabs/tabs';
 })
 export class PagarFase2 {
 
-uDest: FirebaseObjectObservable<any>;
-User: FirebaseObjectObservable<any>;
-transO: FirebaseListObservable<any>;
-transD: FirebaseListObservable<any>;
-saldo: FirebaseObjectObservable<any>;
-trans: FirebaseObjectObservable<any>;
-private usuarioApagar: any;
+  uDest: FirebaseObjectObservable<any>;
+  User: FirebaseObjectObservable<any>;
+  transO: FirebaseListObservable<any>;
+  transD: FirebaseListObservable<any>;
+  saldo: FirebaseObjectObservable<any>;
+  saldo2: FirebaseObjectObservable<any>;
+  trans: FirebaseObjectObservable<any>;
+  private usuarioApagar: any;
 
-f:any;
+  f: any;
 
-clave:any;
-monto:any;
-clavep:any;
-cuenta:any;
-cuentadest:any;
-nombre:any;
-Userid:any;
-userSaldo:any;
-montofinal:any;
+  clave: any;
+  monto: any;
+  clavep: any;
+  cuenta: any;
+  cuentadest: any;
+  nombre: any;
+  Userid: any;
+  userSaldo: any;
+  montofinal: any;
 
-montoactual:any;
+  montoactual: any;
+  montoactual2: any;
 
 
-  constructor(public navCtrl: NavController, 
-                public alertController: AlertController, 
-                  public navParams: NavParams, 
-                    public database: AngularFireDatabase) 
-  {
-      
-       this.usuarioApagar = navParams.get('usuarioApagar');
-       this.User = this.database.object('/User/-KhUY7ugwy_VJqJXIDay'); // Cambiar en login
-       this.transO = this.database.list('/Transacciones/-KhUY7ugwy_VJqJXIDay');
-       this.transD = this.database.list('/Transacciones/'+this.usuarioApagar.id);
-       this.saldo = this.database.object('/Saldo/-KhUY7ugwy_VJqJXIDay/Cuenta1'); // Cambiar en login
+  constructor(public navCtrl: NavController,
+    public alertController: AlertController,
+    public navParams: NavParams,
+    public database: AngularFireDatabase) {
 
+    this.usuarioApagar = navParams.get('usuarioApagar');
+    this.User = this.database.object('/User/-KhUY7ugwy_VJqJXIDay'); // Cambiar en login
+    this.transO = this.database.list('/Transacciones/-KhUY7ugwy_VJqJXIDay');
+    this.transD = this.database.list('/Transacciones/' + this.usuarioApagar.id);
+    this.saldo = this.database.object('/Saldo/-KhUY7ugwy_VJqJXIDay/Cuenta1'); // Cambiar en login
+    this.saldo2 = this.database.object('/Saldo/'+ this.usuarioApagar.id+'/Cuenta1'); 
+
+    this.saldo.subscribe(snapshot => {
+      this.montoactual = snapshot.Saldo;
+    });
+    this.saldo2.subscribe(snapshot => {
+      this.montoactual2 = snapshot.Saldo;
+    });
 
 
   }
@@ -52,94 +60,95 @@ montoactual:any;
   private pagar() {
 
     this.User.subscribe(
-            snapshot => {
+      snapshot => {
 
-            (this.clavep) = snapshot.ClavePago; // Cambiar en login
-            (this.cuenta) = snapshot.Cuentas.Cuenta1.Numero; // Cambiar en login
-            (this.nombre) =snapshot.Nombre; // Cambiar en login
-            (this.Userid) = snapshot.$key;    // Cambiar en login 
+        (this.clavep) = snapshot.ClavePago; // Cambiar en login
+        (this.cuenta) = snapshot.Cuentas.Cuenta1.Numero; // Cambiar en login
+        (this.nombre) = snapshot.Nombre; // Cambiar en login
+        (this.Userid) = snapshot.$key;    // Cambiar en login 
 
 
-            if (this.clavep == this.clave)
-            {
-           //BEWARE, CHIGUIRE INCOMING
-                this.saldo.subscribe(snapshot => {
-                  this.montoactual = snapshot.Saldo;
-                });
+        if (this.clavep == this.clave) {
+          //BEWARE, CHIGUIRE INCOMING
 
-                if(this.montoactual >= this.monto)
-                  {
 
-                      this.transO.push({
-                            CuentaOrigen: this.cuenta,
-                            Monto: '-'+this.monto,
-                            CuentaDestino: this.usuarioApagar.Cuentas.Cuenta1.Numero,
-                          });
+          if (this.montoactual >= this.monto) {
 
-                      this.transD.push({
-                            CuentaOrigen: this.cuenta,
-                            Monto: '+'+this.monto,
-                            CuentaDestino: this.usuarioApagar.Cuentas.Cuenta1.Numero,
-                          });
-                    
-                    let montoguardar = this.montoactual - this.monto;
+            this.transO.push({
+              CuentaOrigen: this.cuenta,
+              Monto: '-' + this.monto,
+              CuentaDestino: this.usuarioApagar.Cuentas.Cuenta1.Numero,
+            });
 
-                    this.saldo.update({
-                      Saldo: montoguardar,
-                    });
+            this.transD.push({
+              CuentaOrigen: this.cuenta,
+              Monto: '+' + this.monto,
+              CuentaDestino: this.usuarioApagar.Cuentas.Cuenta1.Numero,
+            });
+// Transferencia real
+            let montoguardar = this.montoactual - this.monto;
 
-                    let confirmacion = this.alertController.create({
-                      title: "Pago",
-                      message: "Pago realizado exitosamente!",
-                      buttons: [
-                      {
-                        text: "Listo",
-                        handler: data => {
-                          this.navCtrl.popToRoot();
-                        }
-                      }]
-                });
+            this.saldo.update({
+              Saldo: montoguardar,
+            });
 
-                confirmacion.present(confirmacion);
-
-                  }else{
-                    let mensaje = this.alertController.create({
-                      title: "Saldo Insuficiente",
-                      message: "Usted no posee saldo suficiente para esta transaccion!",
-                      buttons: [
-                      {
-                        text: "Ok",
-                        handler: data => {
-                          console.log(' ');
-                        }
-                      }]
-                    });
-                    mensaje.present(mensaje);
+             let montoguardar2 = this.montoactual2 + this.monto;
+             
+              this.saldo2.update({
+              Saldo: montoguardar2,
+            });
+// Transferencia real
+            let confirmacion = this.alertController.create({
+              title: "Pago",
+              message: "Pago realizado exitosamente!",
+              buttons: [
+                {
+                  text: "Listo",
+                  handler: data => {
+                    this.navCtrl.popToRoot();
                   }
-            //End of CHIGUIRE
+                }]
+            });
 
+            confirmacion.present(confirmacion);
 
-            }
-            else 
-            {
-              let negacion = this.alertController.create({
-                  title: "Pago",
-                  message: "La clave secreta no es correcta, vuelve a introducirla por favor!",
-                  buttons: [
-                  {
-                    text: "Ok",
-                    handler: data => {
+          } else {
+            let mensaje = this.alertController.create({
+              title: "Saldo Insuficiente",
+              message: "Usted no posee saldo suficiente para esta transaccion!",
+              buttons: [
+                {
+                  text: "Ok",
+                  handler: data => {
                     console.log(' ');
-                    }
                   }
-                  ]
-              });
-              
-              negacion.present(negacion); 
-            }
-        });
+                }]
+            });
+            mensaje.present(mensaje);
+          }
+          //End of CHIGUIRE
 
-    
+
+        }
+        else {
+          let negacion = this.alertController.create({
+            title: "Pago",
+            message: "La clave secreta no es correcta, vuelve a introducirla por favor!",
+            buttons: [
+              {
+                text: "Ok",
+                handler: data => {
+                  console.log(' ');
+                }
+              }
+            ]
+          });
+
+          negacion.present(negacion);
+        }
+      });
+
+
 
   }
 }
